8c87ffc930befb025f9a5d29f778b9fb86467f4c
Your Name <you@example.com>
2022-09-27 19:55:33 -0600

    confused on 7 but went ahead

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
+from sqlite3 import OperationalError
 import mariadb
 import dbcreds
+
+def connect_db():
+    try:
+        conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password,
+        host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
+        cursor = conn.cursor()
+        return cursor
+    except mariadb OperationalError
